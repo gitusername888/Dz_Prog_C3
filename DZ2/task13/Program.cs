1ed54aec1b821bfd3f416fcc85b063fa5e5c60c0
Di <you@example.com>
2023-08-09 23:20:51 +0300

    Задача 13 start

---
+++ DZ2/task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32( Console.ReadLine() );
+
+
+int digital1 = num % 100;
+int digital2 = num % 10;
+int dig = (digital1 - digital2)/10;
+
+
+Console.WriteLine ($"{num} -> {dig}");
