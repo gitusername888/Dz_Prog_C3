1a10ac36bbda57c7ccda7f5922f76c476187369e
Di <you@example.com>
2023-08-09 23:17:07 +0300

    Задача 10

---
+++ DZ2/task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное целое число");
+int num = Convert.ToInt32( Console.ReadLine() );
+
+
+int digital1 = num % 100;
+int digital2 = num % 10;
+int dig = (digital1 - digital2)/10;
+
+
+Console.WriteLine ($"{num} -> {dig}");
