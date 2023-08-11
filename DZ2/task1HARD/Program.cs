f5d863363402af633b99b256c3e5c2393eed4d0b
Di <you@example.com>
2023-08-11 21:58:45 +0300

    Задача 1hard end

--- DZ2/task1HARD/Program.cs
+++ DZ2/task1HARD/Program.cs
@@ -12,44 +12,25 @@
 Console.WriteLine("Введите число");
 
 string st = Console.ReadLine();
-char [] array;
-for(var i =0; i < st.Length; i++)
+string st1 = st;
+var index = st.IndexOf(",");
+if (index == 1)
 {
-    array [i] = st[i];
+st = st.Remove(2, 1);
+
+Console.WriteLine($"{st1} -> {st}");
+
+}
+else if (st.Length <2)
+{ Console.WriteLine($"{st1} -> нет");}
+
+
+else
+{
+st = st.Remove(1, 1);
+
+Console.WriteLine($"{st1} -> {st}");
 }
 
-Console.WriteLine(array[st.Length]);
-// double number = Convert.ToSingle(st);
 
 
-// Console.WriteLine(number);   // 23,56
-
-
-
-// int result1 = Math.Sign(num1); //-1
-// int i = 0;
-// if (result1 == -1) {
-//     num1 = num1*-1;
-// }
-// if (num1/10 >= 10)
-// {
-    
-//    int k = num1/10;
-//     while (k > 99)
-//     {
-//         k = k/10;
-//         i++;
-//     }
-//     double num2 = Math.Pow(10, i);
-    
-//     double num3 = Math.Floor(num1/num2);
-//     int num4 = Convert.ToInt32 (num3);
-//     int ost100 = num4%10;
-    
-    
-//     Console.WriteLine($"{num1} -> {ost100}");
-// } 
-// else 
-// {
-    // Console.WriteLine($"{num1} -> третий цифры нет ");
-// }
