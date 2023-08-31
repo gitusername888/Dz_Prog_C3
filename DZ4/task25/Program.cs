d5a9535f55aa487707eb7eb55e9a08f8b2f533cc
Di <you@example.com>
2023-08-31 22:47:03 +0300

    Задача 25

---
+++ DZ4/task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+static void ShowCube(int numberA, int numberB)
+    {
+      // Введите свое решение ниже
+
+        double increment = Math.Pow(numberA, numberB);
+        System.Console.WriteLine($"{numberA}, {numberB} -> {increment}");
+    
+    }
+
+
+
+Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32( Console.ReadLine() );
+
+        ShowCube(numberA, numberB);
