d0ac9b9ce3bd619754625bf8740dfe4f82f6719e
Di <you@example.com>
2023-08-31 21:34:23 +0300

    Задача 1

---
+++ DZ3/task2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+
+
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+
+
+
+Console.WriteLine("Введите координаты pointA");
+// int number = Convert.ToInt32( Console.ReadLine() );
+
+
+int pointA [] = {3,6,8};
+int pointB [] = {2,1,-7};
+
+bool DistanceBetweenPoints (int pointA [], int pointB [])
+{
+
+int D = Math.Sqrt((X2-X1)^2 + (Y2-Y1)^2 + (Z2 - Z1)^2)
+
+
+
+// int condition1 = number/10000;
+// int condition2 = number/1000 - condition1*10;
+// int condition3 = number%100/10;
+// int condition4 = number%10;
+// int condition5 = number - 9999;
+
+// if ( condition5 > 0 & condition5 < 99999)
+// {
+//     if ( condition1 == condition4 & condition2 == condition3) 
+//     {
+//         Console.Write($"{number} -> ");
+//         return true;
+        
+//     }
+//     else 
+//     {
+//         Console.Write($"{number} -> ");
+//         return false;
+//     }
+// }
+// else
+// {
+//    Console.WriteLine($"{number} -> Число не пятизначное");
+//     return false;
+// }
+}
+
+
+
+
+
+bool result = IsPalindrome(number);
+
+Console.WriteLine($"{result}");
+
