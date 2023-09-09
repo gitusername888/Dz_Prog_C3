2cf8e30cb70955d0f17a47c74c4b20809c17dbb6
Di <you@example.com>
2023-09-09 21:14:31 +0300

    Задача 1

---
+++ DZ7/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+ double[, ] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom) {
+      // Введите свое решение ниже
+    double[, ] matrix = new double [m,n];
+    int t = 0;
+     for (int i = 0; i < m; i++)
+     {
+        for (int j = 0; j < n; j++)
+        {t = new Random().Next(minLimitRandom,maxLimitRandom);
+        matrix [i,j] = new Random().NextDouble()*t;
+           }
+     }
+ return matrix; 
+  }
+
+   void PrintArray(double[, ] matrix) {
+      // Введите свое решение ниже
+  for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            System.Console.Write($"{matrix[i,j]:f2}\t");
+        System.Console.WriteLine();
+        }
+   
+  }
+
+ 
+      int m = 3;
+      int n = 4;
+      int minLimitRandom = -10;
+      int maxLimitRandom = 10;
+      
+      double[, ] result = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
+      PrintArray(result);
+ 
